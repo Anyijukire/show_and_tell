004a06c505be9c9b47a2a957d260323d9b706a35
Anyijukire <79198971+Anyijukire@users.noreply.github.com>
2021-04-01 08:35:16 +0300

    Update show and tell.kt

--- src/show and tell.kt
+++ src/show and tell.kt
@@ -8,14 +8,17 @@ var Janet= AnitaB("Janet","female",2001)
    println( Janet.name)
 }
 class AnitaB(var name: String, var sex:String, var yearOfBirth:Int){
+    //prints out the introdution of an student in class AnitaB
     fun introduction(myName:String):String{
       return "Hi my name is $myName. I am in Anita B class"
     }
+    //prints out the qualifications to be in AnitaB class
     fun qualification(){
        var qualifyArray=arrayOf("African","female")
         var students= Arrays.toString(qualifyArray)
         println(students)
     }
+    //calculates the age of the student
     fun age():Int{
         var age=2021-yearOfBirth
         return age
